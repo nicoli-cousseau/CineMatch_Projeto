c653c0a40a209eed8637d170aedeb4d6da196a3e
nicoli-cousseau <nicoli.cousseau@rede.ulbra.br>
2026-08-28 15:07:08 -0300

    Minhas atualizações do CineMatch

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("JavaScript conectado!");
+
+document.querySelectorAll(".seta-direita").forEach(function(seta) {
+  console.log(seta);
+});
+
+document.querySelectorAll(".seta-esquerda").forEach(function(seta) {
+    console.log(seta);
+});
+
